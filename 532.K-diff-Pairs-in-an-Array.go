99e22625e1b707f9d482b0382e4c730323932f8b
mickey0524 <buptbh@163.com>
2018-10-06 12:07:20 +0800

    feat(leetcode): add No.532

---
+++ 532.K-diff-Pairs-in-an-Array.go
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/k-diff-pairs-in-an-array/description/
+//
+// algorithms
+// Easy (28.46%)
+// Total Accepted:    46.7k
+// Total Submissions: 164.1k
+// beats 54.84% of golang submissions
+
+func findPairs(nums []int, k int) int {
+	if k < 0 {
+		return 0
+	}
+
+	numMap := make(map[int]int)
+
+	for _, num := range nums {
+		if cntNum, ok := numMap[num]; ok {
+			numMap[num] = cntNum + 1
+		} else {
+			numMap[num] = 1
+		}
+	}
+
+	res := 0
+	for key, value := range numMap {
+		if k == 0 {
+			if value > 1 {
+				res++
+			}
+			continue
+		}
+		if _, ok := numMap[key+k]; ok {
+			res++
+		}
+	}
+
+	return res
+}
