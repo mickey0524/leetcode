5a8a1c8d43424bf4b2d2fb0e9e515d3f84e7bb81
mickey0524 <buptbh@163.com>
2018-10-16 19:50:51 +0800

    feat(leetcode): add No.895 readme

--- 895.Maximum-Frequency-Stack.py
+++ 895.Maximum-Frequency-Stack.py
@@ -5,6 +5,9 @@
 # Total Accepted:    3.8k
 # Total Submissions: 7.9k
 # beats 37.32% of python submissions
+# 据题易得，需要用一个hashmap来保存key的频率，当push一个key的时候，对应key的频率+1
+# 这里巧妙的是用一个dict保存了当前每个频率的key，这样可以方便的得到max_freq_key，因此
+# 在进行pop的时候，进化为O(1)的操作
 
 from collections import Counter, defaultdict
 
