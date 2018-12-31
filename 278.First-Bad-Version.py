e05a73f6775682f0cf9369c8e663f0bf22d9517f
mickey0524 <buptbh@163.com>
2018-12-31 22:14:40 +0800

    feat(leetcode): add No.278

---
+++ 278.First-Bad-Version.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/first-bad-version/
+#
+# algorithms
+# Easy (28.14%)
+# Total Accepted:    189,920
+# Total Submissions: 674,806
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        head, tail, middle = 1, n, None
+        hash_map = {}
+        if n == 1 or isBadVersion(1):
+            return 1
+        
+        while head <= tail:
+            middle = head + (tail - head) / 2
+            
+            isMiddleBad = isBadVersion(middle)
+            if isMiddleBad:
+                if middle - 1 in hash_map and not hash_map[middle - 1]:
+                    return middle
+                tail = middle
+                hash_map[middle] = True
+            elif middle + 1 in hash_map and hash_map[middle + 1]:
+                return middle + 1
+            else:
+                head = middle + 1
+                hash_map[middle] = False
+        
+        return 1
+        
