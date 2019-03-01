4d86273e2d0b305b0ee23784d9afc418d89aba7d
mickey0524 <buptbh@163.com>
2019-03-01 17:19:26 +0800

    feat(leetcode): update No.222

--- 222.Count-Complete-Tree-Nodes.py
+++ 222.Count-Complete-Tree-Nodes.py
@@ -45,4 +45,50 @@ class Solution(object):
             root = root.left
         
         return height
+
+
+# 复习的时候的优化版本
+# 左节点的高度是没有必要计算的，只需要计算第一次
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution1(object):
+    def countNodes(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        res = 0
+        l_h, r_h = None, None
+
+        while root:
+            if l_h:
+                l_h = l_h - 1
+                r_h = self.get_tree_height(root.right)
+            else:
+                l_h, r_h = self.get_tree_height(
+                    root.left), self.get_tree_height(root.right)
+
+            res += 1 << r_h
+            if l_h == r_h:
+                root = root.right
+            else:
+                root = root.left
+
+        return res
+
+    def get_tree_height(self, root):
+        height = 0
+
+        while root:
+            height += 1
+            root = root.left
+
+        return height
+
         
