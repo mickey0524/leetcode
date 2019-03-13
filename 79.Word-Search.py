0af61986daa88d2794acfed1ac9ee03b880ab4ad
mickey0524 <buptbh@163.com>
2019-03-13 22:19:50 +0800

    feat(leetcode): update No.79

--- 79.Word-Search.py
+++ 79.Word-Search.py
@@ -52,3 +52,61 @@ class Solution(object):
                         return True
 
         return False
+
+
+# 效率高很多
+class Solution1(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        def recursive(i, j, idx):
+            if res[0]:
+                return True
+
+            if idx == word_len:
+                res[0] = True
+                return True
+
+            r = False
+
+            if i > 0 and board[i - 1][j] == word[idx] and (i - 1, j) not in is_visited:
+                is_visited[(i - 1, j)] = True
+                tmp = recursive(i - 1, j, idx + 1)
+                r |= tmp
+                del is_visited[(i - 1, j)]
+
+            if i < row - 1 and board[i + 1][j] == word[idx] and (i + 1, j) not in is_visited:
+                is_visited[(i + 1, j)] = True
+                tmp = recursive(i + 1, j, idx + 1)
+                r |= tmp
+                del is_visited[(i + 1, j)]
+
+            if j > 0 and board[i][j - 1] == word[idx] and (i, j - 1) not in is_visited:
+                is_visited[(i, j - 1)] = True
+                tmp = recursive(i, j - 1, idx + 1)
+                r |= tmp
+                del is_visited[(i, j - 1)]
+
+            if j < col - 1 and board[i][j + 1] == word[idx] and (i, j + 1) not in is_visited:
+                is_visited[(i, j + 1)] = True
+                tmp = recursive(i, j + 1, idx + 1)
+                r |= tmp
+                del is_visited[(i, j + 1)]
+
+            return r
+
+        res = [False]
+        word_len = len(word)
+        row, col = len(board), len(board[0])
+
+        for i in xrange(row):
+            for j in xrange(col):
+                if board[i][j] == word[0]:
+                    is_visited = {(i, j): True}
+                    if recursive(i, j, 1):
+                        return True
+
+        return False
