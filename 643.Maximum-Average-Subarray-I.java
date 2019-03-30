21c32f6fe428c7910a2a6e7d1668a6fdef3a11de
mickey0524 <buptbh@163.com>
2019-03-31 00:54:28 +0800

    feat(leetcode): add No.643 java version

---
+++ 643.Maximum-Average-Subarray-I.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/maximum-average-subarray-i/
+//
+// algorithms
+// Easy (39.14%)
+// Total Accepted:    47,855
+// Total Submissions: 122,262
+// beats 100.0% of java submissions
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int curSum = 0;
+        int length = nums.length;
+        
+        for (int i = 0; i < k; i++) {
+            curSum += nums[i];
+        }
+        
+        int res = curSum;
+        
+        for (int i = k, beforeIdx = 0; i < length; i++, beforeIdx++) {
+            curSum = curSum - nums[beforeIdx] + nums[i];
+            res = Math.max(curSum, res);
+        }
+        
+        return (double) res / k;
+    }
+}
