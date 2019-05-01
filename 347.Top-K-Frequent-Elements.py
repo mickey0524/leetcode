d090c8bd752fcadc1995e30a8c58553e9f9214a1
mickey0524 <buptbh@163.com>
2019-05-01 14:06:11 +0800

    feat(leetcode): update No.347

--- 347.Top-K-Frequent-Elements.py
+++ 347.Top-K-Frequent-Elements.py
@@ -16,4 +16,31 @@ class Solution(object):
         """
         counter = Counter(nums)
         return [x[0] for x in counter.most_common(k)]
+
+
+from heapq import heappush, heappop
+
+
+class Solution1(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        hash_map = {}
+        for n in nums:
+            hash_map[n] = hash_map.get(n, 0) + 1
+
+        h = []
+
+        for key, v in hash_map.iteritems():
+            heappush(h, (-v, key))
+
+        res = []
+
+        for _ in xrange(k):
+            res.append(heappop(h)[1])
+
+        return res
         
