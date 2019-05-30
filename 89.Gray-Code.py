3888fca17355115988178e80b2d715ce5605625e
mickey0524 <buptbh@163.com>
2019-05-30 10:38:38 +0800

    feat(leetcode): update No.89

--- 89.Gray-Code.py
+++ 89.Gray-Code.py
@@ -25,3 +25,28 @@ class Solution(object):
             base <<= 1
 
         return res
+
+
+class Solution1(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        if n == 0:
+            return [0]
+
+        res = ['0', '1']
+        zero_prefix = True
+
+        for i in xrange(2, n + 1):
+            tmp = []
+            for n in res:
+                if zero_prefix:
+                    tmp += ['0' + n, '1' + n]
+                else:
+                    tmp += ['1' + n, '0' + n]
+                zero_prefix = not zero_prefix
+            res = tmp
+
+        return [int(n, 2) for n in res]
