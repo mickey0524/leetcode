f33917432d6ed52c8cb1638bc35a586af3ecf3ef
mickey0524 <buptbh@163.com>
2019-07-13 10:12:55 +0800

    feat(leetcode): add No.1116

---
+++ 1116.Print-Zero-Even-Odd.java
@@ -0,0 +1,50 @@
+// https://leetcode.com/problems/print-zero-even-odd/
+//
+// algorithms
+// Medium (45.52%)
+// Total Accepted: 198
+// Total Submissions: 435
+// beats 100.0% of java submissions
+
+
+import java.util.concurrent.Semaphore;
+
+class ZeroEvenOdd {
+    private int n;
+    private Semaphore zero = new Semaphore(1);
+    private Semaphore even = new Semaphore(0);
+    private Semaphore odd = new Semaphore(0);
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            zero.acquire();
+            printNumber.accept(0);
+            if ((i & 1) == 0) {
+                odd.release();
+            } else {
+                even.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            even.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            odd.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+}
