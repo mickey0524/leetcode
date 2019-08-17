5aa4959f3b24ca2a7ba263545729d298649e6123
mickey0524 <buptbh@163.com>
2019-08-17 20:40:50 +0800

    feat(leetcode): update No.621

--- 621.Task-Scheduler.py
+++ 621.Task-Scheduler.py
@@ -44,3 +44,50 @@ class Solution(object):
             res -= last
 
         return res
+
+
+from heapq import heappush, heappop
+
+
+class Solution1(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        task_num = [0] * 26
+
+        for ch in tasks:
+            idx = ord(ch) - 65
+            task_num[idx] += 1
+
+        h = []
+        for num in task_num:
+            if num != 0:
+                heappush(h, -num)
+
+        res = 0
+        while h:
+            tmp = []
+            i = 0
+
+            while i < n + 1:
+                if not h:
+                    break
+
+                num = heappop(h)
+                if num < -1:
+                    tmp.append(num + 1)
+
+                i += 1
+
+            if tmp:
+                res += (n + 1)
+                for num in tmp:
+                    heappush(h, num)
+
+            else:
+                res += i
+
+        return res
