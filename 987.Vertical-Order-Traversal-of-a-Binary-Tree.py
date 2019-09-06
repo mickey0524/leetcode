fdb253094d8537b3e2eb5fd921c83e8deba41ef1
mickey0524 <buptbh@163.com>
2019-09-06 15:34:43 +0800

    feat(leetcode): update No.987

--- 987.Vertical-Order-Traversal-of-a-Binary-Tree.py
+++ 987.Vertical-Order-Traversal-of-a-Binary-Tree.py
@@ -41,3 +41,47 @@ class Solution(object):
             res += tmp,
 
         return res
+
+
+from collections import defaultdict, deque
+
+
+class Solution1(object):
+    def verticalTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        hash_map = defaultdict(list)
+        q = deque([(root, 0, 0)])
+        l, r = 0, 0
+
+        while q:
+            node, x, y = q.popleft()
+            hash_map[x].append((y, node.val))
+            l = min(l, x)
+            r = max(r, x)
+
+            if node.left:
+                q.append((node.left, x - 1, y - 1))
+
+            if node.right:
+                q.append((node.right, x + 1, y - 1))
+
+        res = []
+
+        def compare(a, b):
+            if a[0] == b[0]:
+                return a[1] - b[1]
+
+            return b[0] - a[0]
+
+        for i in xrange(l, r + 1):
+            if i in hash_map:
+                hash_map[i].sort(cmp=compare)
+                res.append([v[1] for v in hash_map[i]])
+
+        return res
