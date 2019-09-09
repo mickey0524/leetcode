83c09faa58a7556cc8dc1ddb911560c26525f593
mickey0524 <buptbh@163.com>
2019-09-09 16:55:08 +0800

    feat(leetcode): add No.912

---
+++ 912.Sort-an-Array.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/sort-an-array/
+#
+# algorithms
+# Medium (63.18%)
+# Total Accepted:    23,269
+# Total Submissions: 36,832
+
+
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+
+        return nums
