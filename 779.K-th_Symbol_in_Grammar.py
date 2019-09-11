68c2c54772690230585f12df7ce74e241741fd2a
mickey0524 <buptbh@163.com>
2019-09-11 10:43:04 +0800

    feat(leetcode): update No.779

--- 779.K-th_Symbol_in_Grammar.py
+++ 779.K-th_Symbol_in_Grammar.py
@@ -36,3 +36,28 @@ class Solution(object):
             cnt_bit = s[idx]
 
         return int(cnt_bit)
+
+
+class Solution1(object):
+    def kthGrammar(self, N, K):
+        """
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        if N == 1:
+            return 0
+
+        cur_len = 2 ** (N - 1)
+        for i in xrange(N, 2, -1):
+            next_len = cur_len >> 1
+            if K > next_len:
+                tmp = next_len >> 1
+                if K <= next_len + tmp:
+                    K -= tmp
+                else:
+                    K -= (next_len + tmp)
+
+            cur_len = next_len
+
+        return 0 if K == 1 else 1
