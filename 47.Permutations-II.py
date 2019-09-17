2a91f5f99d749ad5d4278ab3993a3eb3e468a1b0
mickey0524 <buptbh@163.com>
2019-09-17 17:26:40 +0800

    feat(leetcode): update No.47

--- 47.Permutations-II.py
+++ 47.Permutations-II.py
@@ -14,4 +14,32 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         return [list(t) for t in set(permutations(nums))]
+
+
+class Solution1(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        length = len(nums)
+        if length < 2:
+            return [nums]
+
+        nums.sort()
+        res = []
+
+        for i in xrange(length):
+            if i != 0 and nums[i] == nums[i - 1]:
+                continue
+
+            nums[0], nums[i] = nums[i], nums[0]
+
+            recursive_res = self.permuteUnique(nums[1:])
+            for arr in recursive_res:
+                res.append([nums[0]] + arr)
+
+            nums[0], nums[i] = nums[i], nums[0]
+
+        return res
         
