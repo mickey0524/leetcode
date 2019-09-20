f754f27a82c1ed6d468b44625f86863d864e787b
mickey0524 <buptbh@163.com>
2019-09-20 22:45:17 +0800

    feat(leetcode): add No.394

--- 394.Decode-String.py
+++ 394.Decode-String.py
@@ -57,3 +57,46 @@ class Solution(object):
             res.insert(0, stack.pop())
 
         return ''.join(res)
+
+
+from collections import deque
+
+
+class Solution1(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stack = deque()
+        length = len(s)
+        i = 0
+
+        while i < length:
+            if s[i].isalpha():
+                j = i + 1
+                while j < length and s[j].isalpha():
+                    j += 1
+                stack.append(s[i:j])
+                i = j
+
+            elif s[i].isdigit():
+                j = i + 1
+                while j < length and s[j].isdigit():
+                    j += 1
+                stack.append(int(s[i:j]))
+                i = j + 1
+
+            elif s[i] == ']':
+                tmp = ''
+                while stack and type(stack[-1]) is not int:
+                    tmp = stack[-1] + tmp
+                    stack.pop()
+
+                n = stack.pop()
+                tmp = n * tmp
+                stack.append(tmp)
+
+                i += 1
+
+        return ''.join(stack)
