c865cc5f5916c2765898de12cfee4dd540363d17
mickey0524 <buptbh@163.com>
2019-09-20 22:50:23 +0800

    feat(leetcode): update No.38

--- 38.Count-and-Say.py
+++ 38.Count-and-Say.py
@@ -34,3 +34,31 @@ class Solution(object):
             path = cnt_path
 
         return path
+
+
+class Solution1(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 1:
+            return '1'
+
+        tmp = '1'
+        for i in xrange(2, n + 1):
+            length = len(tmp)
+            i = 0
+            arr = []
+
+            while i < length:
+                j = i + 1
+                while j < length and tmp[j] == tmp[i]:
+                    j += 1
+
+                arr.extend([str(j - i), tmp[i]])
+                i = j
+
+            tmp = ''.join(arr)
+
+        return tmp
