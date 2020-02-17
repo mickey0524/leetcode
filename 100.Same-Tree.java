4d455530df4941a1e9e270c5643b819bc4f63a5a
mickey0524 <buptbh@163.com>
2020-02-17 17:20:36 +0800

    feat(leetcode): add No.100

---
+++ 100.Same-Tree.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/same-tree/
+//
+// algorithms
+// Easy (51.77%)
+// Total Accepted: 480,619
+// Total Submissions: 928,438
+// beats 100.0% of java submissions
+
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode
+ * left; TreeNode right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
