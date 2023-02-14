66580eb4e80adbbbaa1b52dadc8f78649f952f26
wintersnow <buptbh@163.com>
2023-02-14 11:52:04 +0800

    feat(leetcode): add No.2562

---
+++ 2562.Find-the-Array-Concatenation-Value.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/find-the-array-concatenation-value/
+// algorithms
+// Easy (65.15%)
+// Total Accepted: 20.4K
+// Total Submissions: 29.5K
+
+
+class Solution {
+
+    public long findTheArrayConcVal(int[] nums) {
+        
+    }
+    
+}
