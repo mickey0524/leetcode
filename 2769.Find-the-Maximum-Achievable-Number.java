1f4d6bea7baeb57177dc833b4bf75e65c5c96a41
wintersnow <buptbh@163.com>
2023-07-19 11:21:51 +0800

    feat(leetcode): add No.2769

---
+++ 2769.Find-the-Maximum-Achievable-Number.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
+// algorithms
+// Easy (32.6%)
+// Total Accepted: 27.7K
+// Total Submissions: 31.5K
+
+class Solution {
+
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+
+}
